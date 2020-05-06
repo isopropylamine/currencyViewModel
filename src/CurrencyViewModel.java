72fef84044a787b17a20deca203aa261c01e58c9
Christopher Tran <isopropylamine@gmail.com>
2020-05-06 19:15:53 -0400

    Update CurrencyViewModel.java

--- src/CurrencyViewModel.java
+++ src/CurrencyViewModel.java
@@ -2,34 +2,25 @@ import com.ibm.icu.text.NumberFormat;
 import lombok.ToString;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Locale;
+
 
 
 /**
  * Currency view model. Handles currency formatting and displaying.
  */
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
-<<<<<<< Updated upstream
-=======
 
-
->>>>>>> Stashed changes
 public class CurrencyViewModel {
-
     private final BigDecimal amount;
     private final NumberFormat numberFormat;
 
+
     // constructor Test two
     public CurrencyViewModel(double amount, Locale locale) {
-<<<<<<< Updated upstream
-=======
         //The rounded amount will change the currency amount to one with 2 digits after the decimal
         this.amount = new BigDecimal(amount).setScale(2, RoundingMode.HALF_UP);
->>>>>>> Stashed changes
         this.numberFormat = NumberFormat.getCurrencyInstance(locale);
-        //Rounds to the maximum fraction digits of the given locale's currency.
-        this.amount = new BigDecimal(amount).setScale(numberFormat.getMaximumFractionDigits(), RoundingMode.HALF_UP);;
 
         //if the decimal amount is equal to zero, we discard the digits after the decimal
         if(this.amount.remainder(BigDecimal.ONE).compareTo(BigDecimal.ZERO) == 0 ) {
@@ -47,7 +38,7 @@ public class CurrencyViewModel {
 
     @ToString.Include
     public String getAmountWithSymbols() {
-
         return numberFormat.format(amount);
     }
+
 }
